b30f3d99ae360a9f45414399a592887407d9e660
Cassie Rosner <cerosner@gmail.com>
2019-01-17 12:44:16 -0500

    set layout stylesheet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan')
 const express = require('express')
+const layout = require('./views/layout');
 
 const app = express()
 
@@ -7,10 +8,10 @@ app.use(morgan('dev'))
 app.use(express.urlencoded())
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (res, req, next) => {
-    const reqBody = express.urlencoded(req.body)
+app.get('/', (req, res, next) => {
     console.log('Hello world')
 
+    res.send(layout(''));
 })
 
-app.listen(3000, () => console.log("listening on port 3000"))
+app.listen(3000, () => console.log("listening on port 3000"));
